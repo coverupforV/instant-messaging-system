7455cb19edda358b43097a9d83045d7bcbb1a2bd
coverupforV <pcw760404931@163.com>
2023-03-18 21:30:17 +0800

    new file:   main.go 	new file:   server.go

---
+++ server.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+type Server struct {
+	Ip   string
+	Port int
+}
+
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip:   ip,
+		Port: port,
+	}
+
+	return server
+}
+
+func (this *Server) Handler(conn net.Conn) {
+	//当前连接的业务
+	fmt.Println("connect succeed!")
+}
+
+// 启动
+func (this *Server) Start() {
+	//socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Println("net.Listen error!")
+		return
+	}
+	//close listen socket
+	defer listener.Close()
+
+	for {
+		//accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener.Accept error:", err)
+			continue
+		}
+
+		//do handler
+		go this.Handler(conn){
+			
+		}
+	}
+
+}
